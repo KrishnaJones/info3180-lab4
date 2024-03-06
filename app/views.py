7ba858a98d440239deaa8fc7329841cd9c8b2ee9
Krishna Jones <krishna123jones@gmail.com>
2024-03-05 22:09:46 -0500

    Added File Upload form

--- app/views.py
+++ app/views.py
@@ -4,8 +4,9 @@ from flask import render_template, request, redirect, url_for, flash, session, a
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
 from app.models import UserProfile
-from app.forms import LoginForm
+from app.forms import LoginForm, UploadForm
 
 
 ###
@@ -25,17 +26,24 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
     # Instantiate your form class
+    uploads = UploadForm()
 
+    if request.method == 'POST':
     # Validate file upload on submit
-    if form.validate_on_submit():
-        # Get file data and save to your uploads folder
+        if uploads.validate_on_submit():
+            # Get file data and save to your uploads folder
+            upload = uploads.upload.data
+            filename = secure_filename(upload.filename)
+            upload.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-        flash('File Saved', 'success')
-        return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
 
-    return render_template('upload.html')
+            flash('File Saved', 'success')
+            return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
+
+    return render_template('upload.html', form=uploads)
 
 
 @app.route('/login', methods=['POST', 'GET'])
